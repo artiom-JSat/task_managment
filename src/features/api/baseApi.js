f2a5e25fbdb261c796181fae3bc59c02a2884c5b
Artiom Sviridovich <jsat1987@gmail.com>
2025-03-26 12:01:48 +0100

    feat-4: create store, baseApi, productApi, ProductList

---
+++ src/features/api/baseApi.js
@@ -0,0 +1,9 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const baseUrl = 'https://fakestoreapi.com/'
+
+export const baseApi = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  tagTypes: ['Products'],
+  endpoints: () => ({}),
+})
