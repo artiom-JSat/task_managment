da65f63ddbee11c55b348057f8e27a3a77ae08c9
Artiom Sviridovich <jsat1987@gmail.com>
2025-04-10 13:55:19 +0200

    feat-7: created CreateProduct, deleting products

--- src/features/products/ProductsList.js
+++ src/features/products/ProductsList.js
@@ -1,9 +1,22 @@
 import React from 'react'
-import { useGetProductsQuery } from '../api/productsApi'
+import { productsApi, useGetProductsQuery } from '../api/productsApi'
 import ProductListItem from './ProductListItem'
-
+import { useDispatch } from 'react-redux'
 const ProductsList = () => {
   const { data: products, error, isLoading } = useGetProductsQuery()
+  const dispatch = useDispatch()
+
+  const handleDeleteCharacter = (id) => {
+    dispatch(
+      productsApi.util.updateQueryData(
+        'getProducts',
+        undefined,
+        (draft) => {
+          return draft.filter((product) => product.id !== id)
+        },
+      ),
+    )
+  }
 
   if (isLoading) return <h1>Loading...</h1>
   if (error)
@@ -13,12 +26,17 @@ const ProductsList = () => {
       </p>
     )
 
+  console.log('products', products)
+
   return (
     <>
       <h1>Products</h1>
       {products.map((product) => (
         <div key={product.id}>
           <ProductListItem product={product} />
+          <button onClick={() => handleDeleteCharacter(product.id)}>
+            Delete
+          </button>
         </div>
       ))}
     </>
